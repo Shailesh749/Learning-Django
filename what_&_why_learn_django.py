1536a12d644134d7eb9dd918bf676e65986209d8
Shailesh Kumar <shailesh7492966727@gmial.com>
2025-08-26 00:11:11 +0530

    what and why learn django in python

---
+++ what_&_why_learn_django.py
@@ -0,0 +1,43 @@
+# What is a Framework?
+
+'''
+A framework is a particular set of rules, ideas, or beliefs which you use in order to deal with problems or to decide what to do.
+'''
+
+
+# What is Django?
+'''
+1 Django is basically a high-level python web application framework that enables the rapid development of web applications.
+
+2 Open-source Python frameworks
+
+3 Follows the model-view-template (MVT) architectural pattern.
+
+4 Most popular Python frameworks
+'''
+
+
+# Why Django Use ?
+'''
+1 it's fast and simple
+
+2 it's secure
+
+3 It suits any web application project
+
+4 It's well-established
+
+5 MVT Support and Object-Oriented Approach
+
+6 Built-in Authentication and Authorization
+
+7 Packaging System
+'''
+
+
+
+# Which companies uses django ?
+'''
+1 YouTube  2 Disqus 3 Instagram 4 Spotify  5 The Washington Post 
+6 Bitbucket 7 Dropbox 8 Mozila
+'''
